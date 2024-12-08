3294c6542649ca388a4815c8b986b80023ae4668
Prashant Prajul <prajulofficial3125@gmail.com>
2024-12-08 18:01:31 +0530

    Serving HTML , CSS and JS (VIEWS)

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 const express=require('express');
 const app=express();
+const path=require('path');
 const bodyParser=require('body-parser');
 
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+
+
+app.use(express.static(path.join(__dirname,'public')))//Here we have added the public folder which can be easily accessed in html files
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next)=>{
-    res.send(`<form action='/product' method="POST">
-        <input type="text" name='title'>
-        <button type="submit"> Add-Product</button>
-        </form>`)
+app.use('/admin',adminRoutes);//by his we can filter path (means the path having /admin will enter this file)
+app.use(shopRoutes);
+app.use((req,res)=>{
+
+res.sendFile(path.join(__dirname,'views','404.html'));
 })
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1> Hello from server</h1>');
-})
-
-app.listen(3000,()=>{
+app.listen(3000,()=>{ 
     console.log("server is running");
 })
